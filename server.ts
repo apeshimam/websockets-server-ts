417b7217dd050cf2c57a6b5ddb9624d1c96717b2
Artie Pesh-Imam <apeshimam@gmail.com>
2022-11-21 14:16:07 -0500

    Initial Commit

---
+++ server.ts
@@ -0,0 +1,26 @@
+import { WebSocketServer, WebSocket } from "ws"
+import * as Automerge from "@automerge/automerge"
+
+const port = 3000
+
+const wss = new WebSocketServer({port})
+const clients = new Set<WebSocket>
+
+type D = { 
+    text: Automerge.Text
+}
+
+var document: Automerge.Doc<D>
+var syncState: Automerge.SyncState
+
+wss.on('connection', ws => {
+    clients.add(ws)
+    ws.on('message', msg => {
+        console.log('Received message from client', msg)
+        clients.forEach( client => {
+            client.send(msg)
+        })
+    })
+})
+
+console.log("Listening on ", port)
