8880ec1bd0568942476cda8dd8224fe3cdab9ad2
Artie Pesh-Imam <apeshimam@gmail.com>
2022-11-28 01:32:53 -0500

    wip

--- server.ts
+++ server.ts
@@ -1,27 +1,18 @@
-import { WebSocketServer, WebSocket } from "ws"
-import * as Automerge from "@automerge/automerge"
+import { WebSocketServer, WebSocket } from "ws";
 
-const port = 3000
+const port = 3000;
 
-const wss = new WebSocketServer({port})
-const clients = new Set<WebSocket>
+const wss = new WebSocketServer({ port });
+const clients = new Set<WebSocket>();
 
-type D = { 
-    text: Automerge.Text
-}
+wss.on("connection", (ws) => {
+  clients.add(ws);
+  ws.on("message", (msg) => {
+    console.log("Received message from client", msg);
+    clients.forEach((client) => {
+      if (client != ws && client.readyState == WebSocket.OPEN) client.send(msg);
+    });
+  });
+});
 
-var document: Automerge.Doc<D>
-var syncState: Automerge.SyncState
-
-wss.on('connection', ws => {
-    clients.add(ws)
-    ws.on('message', msg => {
-        console.log('Received message from client', msg)
-        clients.forEach( client => {
-            if(client != ws && client.readyState == WebSocket.OPEN)
-                client.send(msg)
-        })
-    })
-})
-
-console.log("Listening on ", port)
+console.log("Listening on ", port);
